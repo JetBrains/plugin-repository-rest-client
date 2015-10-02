db5b07b24b0212f3c7c9354a16341497bba2d62a
nik <Nikolay.Chashnikov@jetbrains.com>
2015-10-02 17:21:39 +0300

    updated to Kotlin M13

--- cli/src/main/kotlin/uploader.kt
+++ cli/src/main/kotlin/uploader.kt
@@ -3,32 +3,31 @@ package org.jetbrains.intellij.pluginRepository
 import java.io.File
 import com.sampullara.cli.Argument
 import com.sampullara.cli.Args
-import kotlin.platform.platformStatic
 
 /**
  * @author nik
  */
 public class Uploader {
-    Argument(description = "Plugins repository host")
+    @Argument(description = "Plugins repository host")
     var host = "http://plugins.jetbrains.com"
 
-    Argument(description = "Plugin channel")
+    @Argument(description = "Plugin channel")
     var channel: String? = null
 
-    Argument(required = true)
+    @Argument(required = true)
     var username: String? = null
 
-    Argument(required = true)
+    @Argument(required = true)
     var password: String? = null
 
-    Argument("plugin", required = true, description = "Plugin ID in the plugins repository")
+    @Argument("plugin", required = true, description = "Plugin ID in the plugins repository")
     var pluginId: Int? = null
 
-    Argument("file", required = true, description = "Path to plugin zip/jar file")
+    @Argument("file", required = true, description = "Path to plugin zip/jar file")
     var pluginPath: String? = null
 
     companion object {
-        platformStatic
+        @JvmStatic
         fun main(args: Array<String>) {
             val options = Uploader()
             Args.parseOrExit(options, args)
--- rest/src/main/kotlin/pluginRepositoryRest.kt
+++ rest/src/main/kotlin/pluginRepositoryRest.kt
@@ -23,24 +23,24 @@ public class PluginRepositoryInstance(val siteUrl: String, private val username:
             .setClient({ -> object: UrlConnectionClient() {
                 override fun openConnection(request: Request?): HttpURLConnection {
                     val connection = super.openConnection(request)
-                    connection.setInstanceFollowRedirects(false)
+                    connection.instanceFollowRedirects = false
                     val timeout = 10 * 60 * 1000
-                    connection.setReadTimeout(timeout)
+                    connection.readTimeout = timeout
                     return connection
                 }
             }})
             .build()
-            .create(javaClass<PluginRepositoryService>())
+            .create(PluginRepositoryService::class.java)
 
     fun uploadPlugin(pluginId: Int, file: File, channel: String? = null) {
         try {
-            LOG.info("Uploading plugin $pluginId from ${file.getAbsolutePath()} to $siteUrl")
+            LOG.info("Uploading plugin $pluginId from ${file.absolutePath} to $siteUrl")
             service.upload(TypedString(username), TypedString(password), TypedString(pluginId.toString()),
                     channel?.let { TypedString(it) },
                     TypedFile("application/octet-stream", file))
         }
         catch(e: RetrofitError) {
-            if (e.getResponse()?.getStatus() == 302) {
+            if (e.response?.status == 302) {
                 LOG.info("Uploaded successfully")
                 return
             }
@@ -52,9 +52,9 @@ public class PluginRepositoryInstance(val siteUrl: String, private val username:
 private val LOG = LoggerFactory.getLogger("plugin-repository-rest-client")
 
 private interface PluginRepositoryService {
-    Multipart
-    POST("/plugin/uploadPlugin")
-    fun upload(Part("userName") username: TypedString, Part("password") password: TypedString,
-               Part("pluginId") pluginId: TypedString, Part("channel") channel: TypedString?,
-               Part("file") file: TypedFile): Response
+    @Multipart
+    @POST("/plugin/uploadPlugin")
+    fun upload(@Part("userName") username: TypedString, @Part("password") password: TypedString,
+               @Part("pluginId") pluginId: TypedString, @Part("channel") channel: TypedString?,
+               @Part("file") file: TypedFile): Response
 }
