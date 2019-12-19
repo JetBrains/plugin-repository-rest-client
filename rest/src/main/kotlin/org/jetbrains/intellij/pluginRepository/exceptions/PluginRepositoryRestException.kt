c2bda1429b9719ca4167fb3966809c958ef593fb
Prendota <Prendota@mail.ru>
2019-12-19 14:22:59 +0300

    rename exception

--- rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/exceptions/PluginRepositoryRestException.kt
+++ rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/exceptions/PluginRepositoryRestException.kt
@@ -1,5 +1,5 @@
 package org.jetbrains.intellij.pluginRepository.exceptions
 
-class PluginPluginRepositoryExceptionRepositoryRestException(message: String?, cause: Throwable?) : Exception(message, cause)
+class PluginRepositoryException(message: String?, cause: Throwable?) : Exception(message, cause)
 
-fun restException(message: String?, cause: Throwable? = null): Nothing = throw PluginPluginRepositoryExceptionRepositoryRestException(message, cause)
+fun restException(message: String?, cause: Throwable? = null): Nothing = throw PluginRepositoryException(message, cause)
