9298f265adf28842e69de8a125323cb53d65144d
Prendota <Prendota@mail.ru>
2019-12-19 11:54:09 +0300

    drop code duplicate

--- rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/utils/RequestHelper.kt
+++ rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/utils/RequestHelper.kt
@@ -6,31 +6,14 @@ import org.jetbrains.intellij.pluginRepository.LOG
 import org.jetbrains.intellij.pluginRepository.exceptions.PluginUploadRestError
 import org.jetbrains.intellij.pluginRepository.exceptions.restException
 import retrofit2.Call
+import retrofit2.Response
 import java.io.File
 import java.net.HttpURLConnection
 
-internal fun <T> getResponseOrNull(callable: Call<T>): T? {
-  return try {
-    val executed = callable.execute()
-    if (executed.isSuccessful) executed.body()
-    else null
-  }
-  catch (e: Exception) {
-    LOG.error(e.message, e)
-    restException(e.message, e)
-  }
-}
+internal fun <T> getResponseOrNull(callable: Call<T>): T? = executeAndCall(callable) { it.body() }
 
-internal fun getFileOrNull(callable: Call<ResponseBody>, targetPath: String): File? {
-  return try {
-    val executed = callable.execute()
-    if (executed.isSuccessful) downloadFile(executed, targetPath)
-    else null
-  }
-  catch (e: Exception) {
-    LOG.error(e.message, e)
-    restException(e.message, e)
-  }
+internal fun getFileOrNull(callable: Call<ResponseBody>, targetPath: String): File? = executeAndCall(callable) {
+  downloadFile(it, targetPath)
 }
 
 internal fun <T> uploadOrFail(callable: Call<T>, plugin: String? = null): T {
@@ -45,6 +28,18 @@ internal fun <T> uploadOrFail(callable: Call<T>, plugin: String? = null): T {
   catch (e: Exception) {
     LOG.error(e.message, e)
     restException(e.message)
+  }
+}
+
+private fun <T, R> executeAndCall(callable: Call<T>, block: (response: Response<T>) -> R): R? {
+  return try {
+    val executed = callable.execute()
+    if (executed.isSuccessful) block(executed)
+    else null
+  }
+  catch (e: Exception) {
+    LOG.error(e.message, e)
+    restException(e.message, e)
   }
 }
 
