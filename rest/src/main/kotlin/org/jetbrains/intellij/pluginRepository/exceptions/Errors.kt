be282aabfb91ed2aa1c72dffde59f4375448db38
Prendota <Prendota@mail.ru>
2019-12-20 15:20:51 +0300

    deprecate msg field in RestError class

--- rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/exceptions/Errors.kt
+++ rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/exceptions/Errors.kt
@@ -3,4 +3,8 @@ package org.jetbrains.intellij.pluginRepository.exceptions
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class PluginUploadRestError(val msg: String = "")
+data class PluginUploadRestError(
+  @Deprecated("No longer support in Marketplace REST API", replaceWith = ReplaceWith("message"), level = DeprecationLevel.WARNING)
+  val msg: String? = null,
+  val message: String? = null
+)
