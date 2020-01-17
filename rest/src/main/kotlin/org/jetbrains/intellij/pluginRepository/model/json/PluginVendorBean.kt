29092927c45b183719c6c4d4a3a47a63f53244fa
Prendota <Prendota@mail.ru>
2020-01-17 11:56:21 +0300

    correct vendor bean object

--- rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/model/json/PluginVendorBean.kt
+++ rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/model/json/PluginVendorBean.kt
@@ -3,7 +3,4 @@ package org.jetbrains.intellij.pluginRepository.model.json
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class PluginVendorBean(
-  var name: String? = null,
-  var url: String? = null
-)
+data class PluginVendorBean(val name: String, val url: String?)
