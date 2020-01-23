dcee0572c2e1045b76e4270194c97d314dcd426f
Prendota <Prendota@mail.ru>
2020-01-23 11:50:48 +0300

    docs: clean up after review

--- rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/PluginRepository.kt
+++ rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/PluginRepository.kt
@@ -20,7 +20,7 @@ interface PluginRepository {
 interface PluginManager {
   /**
    * Plugin info by [xmlId] & [ProductFamily].
-   * @param xmlId - id from plugin descriptor file. Example: "org.jetbrains.kotlin"
+   * @param xmlId id from plugin descriptor file. Example: "org.jetbrains.kotlin"
    */
   fun getPluginByXmlId(xmlId: String, family: ProductFamily = ProductFamily.INTELLIJ): PluginBean?
 
@@ -60,15 +60,15 @@ interface PluginManager {
   /**
    * List of plugins xml ids compatible with [build].
    * Supported for [ProductFamily.INTELLIJ].
-   * @param max   - max result set. Max: 10000 - [offset]
-   * @param query - query for search.
+   * @param max max result set. Max: 10000 - [offset]
+   * @param query query for search.
    */
   fun getCompatiblePluginsXmlIds(build: String, max: Int, offset: Int, query: String = ""): List<String>
 
   /**
    * Search last compatible update for each id from [xmlIds]
    * Supported for [ProductFamily.INTELLIJ].
-   * @param channel - plugin channel. Default value is "stable" plugin channel.
+   * @param channel plugin channel. Default value is "stable" plugin channel.
    * @return the list of last compatible updates [CompatibleUpdateBean] for plugins from [xmlIds].
    */
   fun searchCompatibleUpdates(xmlIds: List<String>, build: String, channel: String = ""): List<CompatibleUpdateBean>
@@ -99,17 +99,17 @@ interface PluginUpdateManager {
 interface PluginDownloader {
   /**
    * Download [ProductFamily.INTELLIJ] plugin by plugin XML id.
-   * @param xmlId   - plugin XML id. Example: "org.jetbrains.kotlin" for Kotlin plugin.
-   * @param version - version of the plugin. Example: "1.3.61-release-IJ2019.3-1" for Kotlin plugin.
-   * @param channel - plugin channel. Default value is "stable" plugin channel.
+   * @param xmlId plugin XML id. Example: "org.jetbrains.kotlin" for Kotlin plugin.
+   * @param version version of the plugin. Example: "1.3.61-release-IJ2019.3-1" for Kotlin plugin.
+   * @param channel plugin channel. Default value is "stable" plugin channel.
    */
   fun download(xmlId: String, version: String, targetPath: File, channel: String? = null): File?
 
   /**
    * Download latest compatible update for plugin [ProductFamily.INTELLIJ] by IDE Version.
-   * @param xmlId    - plugin XML id.
-   * @param ideBuild - IDE version. Example: "IC-145.184"
-   * @param channel  - plugin channel. Default value is "stable" plugin channel.
+   * @param xmlId plugin XML id.
+   * @param ideBuild IDE version. Example: "IC-145.184"
+   * @param channel plugin channel. Default value is "stable" plugin channel.
    */
   fun downloadLatestCompatiblePlugin(xmlId: String, ideBuild: String, targetPath: File, channel: String? = null): File?
 }
@@ -117,27 +117,27 @@ interface PluginDownloader {
 interface PluginUploader {
   /**
    * Upload plugin by ID into specific channel.
-   * IMPORTANT: PLUGIN NOTES WILL BE IGNORED FOR IDES BASED ON INTELLIJ PLATFORM ([ProductFamily.INTELLIJ]).
+   * *Important*: plugin notes will be ignored for ides based on intellij platform ([ProductFamily.INTELLIJ]).
    * Plugin notes for teamcity plugins and hub widgets only. For IDE plugins use <changed-notes> element in plugin.xml
-   * @param channel   - plugin channel. Default value is "stable" plugin channel.
-   * @param notes     - plugin update notes.
+   * @param channel plugin channel. Default value is "stable" plugin channel.
+   * @param notes plugin update notes.
    */
   fun uploadPlugin(id: Int, file: File, channel: String? = null, notes: String? = null)
 
   /**
    * Upload plugin by Xml id into specific channel.
-   * IMPORTANT: PLUGIN NOTES WILL BE IGNORED FOR IDES BASED ON INTELLIJ PLATFORM ([ProductFamily.INTELLIJ]).
+   * *Important*: plugin notes will be ignored for ides based on intellij platform ([ProductFamily.INTELLIJ]).
    * Plugin notes for teamcity plugins and hub widgets only. For IDE plugins use <changed-notes> element in plugin.xml
-   * @param channel   - plugin channel. Default value is "stable" plugin channel.
-   * @param notes     - plugin update notes.
+   * @param channel plugin channel. Default value is "stable" plugin channel.
+   * @param notes plugin update notes.
    */
   fun uploadPlugin(xmlId: String, file: File, channel: String? = null, notes: String? = null)
 
   /**
    * Upload a new plugin into JetBrains Marketplace.
    * Make sure you accepted all agreements on the Marketplace web-site: https://plugins.jetbrains.com/.
-   * @param categoryId - tag id. Example: https://plugins.jetbrains.com/idea.
-   * @param licenseUrl - link to the license.
+   * @param categoryId tag id. Example: https://plugins.jetbrains.com/idea.
+   * @param licenseUrl link to the license.
    */
   fun uploadNewPlugin(
     file: File,
