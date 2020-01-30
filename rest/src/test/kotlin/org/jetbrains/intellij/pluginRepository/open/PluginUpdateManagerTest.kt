a8e12b014ded87738e5cc0137b64755ba5d801ee
Prendota <Prendota@mail.ru>
2020-01-30 11:02:06 +0300

    test: add for update manager

--- rest/src/test/kotlin/org/jetbrains/intellij/pluginRepository/open/PluginUpdateManagerTest.kt
+++ rest/src/test/kotlin/org/jetbrains/intellij/pluginRepository/open/PluginUpdateManagerTest.kt
@@ -10,6 +10,8 @@ class PluginUpdateManagerTest : BaseTest() {
   private val service = instance.pluginUpdateManager
   private val pluginService = instance.pluginManager
 
+  private val unknownUpdateId = 39
+
   @Test
   fun `get update by id`() {
     val testPlugin = TestPlugins.GO
@@ -18,6 +20,12 @@ class PluginUpdateManagerTest : BaseTest() {
     Assert.assertTrue(update!!.version == "181.4668.90")
     val plugin = pluginService.getPlugin(update.pluginId)
     Assert.assertTrue(plugin?.name == testPlugin.pluginName)
+  }
+
+  @Test
+  fun `get update for unknown id`() {
+    val update = service.getUpdateById(unknownUpdateId)
+    Assert.assertNull(update)
   }
 
   @Test
@@ -31,6 +39,12 @@ class PluginUpdateManagerTest : BaseTest() {
   }
 
   @Test
+  fun `get by version & family for unknown id`() {
+    val updates = service.getUpdatesByVersionAndFamily("testPlugin.xmlId", "193.5233.13")
+    Assert.assertTrue(updates.isEmpty())
+  }
+
+  @Test
   fun `get intellij meta`() {
     val testPlugin = TestPlugins.KOTLIN
     val meta = service.getIntellijUpdateMetadata(testPlugin.id, testPlugin.updates.first())
@@ -38,6 +52,76 @@ class PluginUpdateManagerTest : BaseTest() {
     Assert.assertTrue(meta?.name == testPlugin.pluginName)
     Assert.assertTrue(meta?.vendor == "JetBrains")
     Assert.assertTrue(meta?.xmlId == testPlugin.xmlId)
+  }
+
+  @Test
+  fun `get intellij meta for unknown id`() {
+    val testPlugin = TestPlugins.KOTLIN
+    val meta = service.getIntellijUpdateMetadata(testPlugin.id, unknownUpdateId)
+    Assert.assertNull(meta)
+  }
+
+
+  @Test
+  fun `get intellij meta batch`() {
+    val testPlugin = TestPlugins.KOTLIN
+    val testPlugin1 = TestPlugins.GO
+    val metas = service.getIntellijUpdateMetadataBatch(listOf(
+      Pair(testPlugin.id, testPlugin.updates.first()),
+      Pair(testPlugin1.id, testPlugin1.updates.first())
+    ))
+    Assert.assertTrue(metas.isNotEmpty())
+    Assert.assertTrue(metas.size == 2)
+
+    val meta1 = metas[testPlugin.updates.first()]
+    Assert.assertNotNull(meta1)
+    Assert.assertTrue(meta1?.name == testPlugin.pluginName)
+    Assert.assertTrue(meta1?.vendor == "JetBrains")
+    Assert.assertTrue(meta1?.xmlId == testPlugin.xmlId)
+
+    val meta2 = metas[testPlugin1.updates.first()]
+    Assert.assertNotNull(meta2)
+    Assert.assertTrue(meta2?.name == testPlugin1.pluginName)
+    Assert.assertTrue(meta2?.vendor == "JetBrains")
+    Assert.assertTrue(meta2?.xmlId == testPlugin1.xmlId)
+  }
+
+  @Test
+  fun `get intellij meta batch for unknown 1`() {
+    val testPlugin = TestPlugins.KOTLIN
+    val testPlugin1 = TestPlugins.GO
+    val metas = service.getIntellijUpdateMetadataBatch(listOf(
+      Pair(testPlugin.id, testPlugin.updates.first()),
+      Pair(testPlugin1.id, unknownUpdateId)
+    ))
+    Assert.assertTrue(metas.isNotEmpty())
+    Assert.assertTrue(metas.size == 1)
+
+    val meta1 = metas[testPlugin.updates.first()]
+    Assert.assertNotNull(meta1)
+    Assert.assertTrue(meta1?.name == testPlugin.pluginName)
+    Assert.assertTrue(meta1?.vendor == "JetBrains")
+    Assert.assertTrue(meta1?.xmlId == testPlugin.xmlId)
+
+    val meta2 = metas[testPlugin1.updates.first()]
+    Assert.assertNull(meta2)
+  }
+
+  @Test
+  fun `get intellij meta batch for unknown ids`() {
+    val testPlugin = TestPlugins.KOTLIN
+    val testPlugin1 = TestPlugins.GO
+    val metas = service.getIntellijUpdateMetadataBatch(listOf(
+      Pair(testPlugin.id, unknownUpdateId),
+      Pair(testPlugin1.id, unknownUpdateId)
+    ))
+    Assert.assertTrue(metas.isEmpty())
+
+    val meta1 = metas[testPlugin.updates.first()]
+    Assert.assertNull(meta1)
+
+    val meta2 = metas[testPlugin1.updates.first()]
+    Assert.assertNull(meta2)
   }
 
 }
