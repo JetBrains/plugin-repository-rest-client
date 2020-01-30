6b0be10ad0e607e1c9774c25c4cd8eab3f62bc60
Prendota <Prendota@mail.ru>
2020-01-30 16:50:06 +0300

    chore: use default fields for IntellijUpdateMetadata

--- rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/model/IntellijUpdateMetadata.kt
+++ rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/model/IntellijUpdateMetadata.kt
@@ -1,19 +1,21 @@
 package org.jetbrains.intellij.pluginRepository.model
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class IntellijUpdateMetadata(
   val id: UpdateId,
   val xmlId: PluginXmlId,
   val name: String,
   val description: String,
   val tags: List<String>,
-  val vendor: String,
-  val version: String,
-  val notes: String,
-  val dependencies: Set<String>,
-  val since: String,
-  val until: String,
-  val productCode: String?,
-  val sourceCodeUrl: String?
+  val vendor: String = "",
+  val version: String = "",
+  val notes: String = "",
+  val dependencies: Set<String> = emptySet(),
+  val since: String? = null,
+  val until: String? = null,
+  val productCode: String? = null,
+  val sourceCodeUrl: String? = null
 )
 
