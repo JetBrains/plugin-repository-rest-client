68198ef2103c18e54d76dc61ce1b81117a26c86a
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2020-03-06 18:39:29 +0300

    Use fixed thread pool for retrofit connections and close it gracefully.

--- rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/internal/api/PluginRepositoryInstance.kt
+++ rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/internal/api/PluginRepositoryInstance.kt
@@ -28,36 +28,46 @@ internal class PluginRepositoryInstance(siteUrl: String, private val token: Stri
 
   private val maxParallelConnection = System.getProperty("MARKETPLACE_MAX_PARALLEL_CONNECTIONS", "16").toInt()
 
-  private val dispatcher = Dispatcher(Executors.newCachedThreadPool(DaemonThreadFactory("retrofit-thread"))).apply {
+  private val executorService = Executors.newFixedThreadPool(maxParallelConnection, DaemonThreadFactory("retrofit-thread"))
+
+  private val dispatcher = Dispatcher(executorService).apply {
     this.maxRequestsPerHost = maxParallelConnection
     this.maxRequests = maxParallelConnection
   }
 
+  private val okHttpClient = OkHttpClient()
+    .newBuilder()
+    .dispatcher(dispatcher)
+    .connectTimeout(5, TimeUnit.MINUTES)
+    .readTimeout(5, TimeUnit.MINUTES)
+    .writeTimeout(5, TimeUnit.MINUTES)
+    .addInterceptor(object : Interceptor {
+      override fun intercept(chain: Interceptor.Chain): Response {
+        val request = if (token != null) {
+          chain.request().newBuilder().addHeader("Authorization", "Bearer $token").build()
+        } else {
+          chain.request()
+        }
+        return chain.proceed(request)
+      }
+    })
+    .build()
+
   private val service = Retrofit.Builder()
     .baseUrl(siteUrl)
-    .client(
-      OkHttpClient()
-        .newBuilder()
-        .dispatcher(dispatcher)
-        .connectTimeout(5, TimeUnit.MINUTES)
-        .readTimeout(5, TimeUnit.MINUTES)
-        .writeTimeout(5, TimeUnit.MINUTES)
-        .addInterceptor(object : Interceptor {
-          override fun intercept(chain: Interceptor.Chain): Response {
-            val request = if (token != null) {
-              chain.request().newBuilder().addHeader("Authorization", "Bearer $token").build()
-            }
-            else {
-              chain.request()
-            }
-            return chain.proceed(request)
-          }
-        })
-        .build())
+    .client(okHttpClient)
     .addConverterFactory(JaxbConverterFactory.create())
     .addConverterFactory(JacksonConverterFactory.create(jacksonObjectMapper()))
     .build()
     .create(PluginRepositoryService::class.java)
+
+  init {
+    Runtime.getRuntime().addShutdownHook(Thread {
+      dispatcher.executorService.shutdownNow()
+      okHttpClient.connectionPool.evictAll()
+      okHttpClient.cache?.close()
+    })
+  }
 
   override val downloader: PluginDownloader = PluginDownloaderInstance(service)
 
