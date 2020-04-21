611010df7a78e904773ae6e84a65f5e3d09bb787
Prendota <Prendota@mail.ru>
2020-04-21 18:47:57 +0300

    fix: add default values to bean

--- rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/model/PluginVendorBean.kt
+++ rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/model/PluginVendorBean.kt
@@ -3,4 +3,4 @@ package org.jetbrains.intellij.pluginRepository.model
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class PluginVendorBean(val name: String, val url: String?)
+data class PluginVendorBean(val name: String = "", val url: String? = null)
