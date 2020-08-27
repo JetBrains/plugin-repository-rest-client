5ad470266373d87869e5cf53e75d62f646ca428f
Prendota <Prendota@mail.ru>
2020-08-27 20:07:53 +0300

    chore: add JsonIgnoreProperties
    GitOrigin-RevId: 1da4638a4a6cd8c775325e58a88db78ad1989f77

--- rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/model/UpdateBean.kt
+++ rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/model/UpdateBean.kt
@@ -1,5 +1,8 @@
 package org.jetbrains.intellij.pluginRepository.model
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class UpdateBean(
   val id: UpdateId,
   val pluginId: PluginId,
