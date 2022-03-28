13fec03691a3809eef90f299edeaa92975393b9c
Prendota <Prendota@mail.ru>
2022-03-28 11:55:24 +0200

    fix: update LicenseUrl
    GitOrigin-RevId: a345bd5ffe90b0981e8aa9dbf219bd9deb221839

--- rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/model/LicenseUrl.kt
+++ rest/src/main/kotlin/org/jetbrains/intellij/pluginRepository/model/LicenseUrl.kt
@@ -2,7 +2,7 @@ package org.jetbrains.intellij.pluginRepository.model
 
 class LicenseUrl private constructor(val url: String) {
   companion object {
-    val JETBRAINS_TERM_OF_USE = LicenseUrl("https://www.jetbrains.com/legal/docs/toolbox/user.html")
+    val JETBRAINS_TERM_OF_USE = LicenseUrl("https://jb.gg/legal/docs/toolbox/user.html")
     val APACHE_2_0 = LicenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
     val MIT = LicenseUrl("https://opensource.org/licenses/MIT")
     val GPL = LicenseUrl("https://www.gnu.org/licenses/gpl-3.0.en.html")
@@ -10,7 +10,7 @@ class LicenseUrl private constructor(val url: String) {
     val ECLIPSE_PUBLIC = LicenseUrl("https://www.eclipse.org/legal/epl-2.0/")
     val GNU_LESSER = LicenseUrl("https://www.gnu.org/licenses/lgpl-3.0.en.html")
     val BSD_3_CLAUSE = LicenseUrl("https://opensource.org/licenses/BSD-3-Clause")
-    val BSD_2_CLAUSE = LicenseUrl("ttps://opensource.org/licenses/BSD-2-Clause")
+    val BSD_2_CLAUSE = LicenseUrl("https://opensource.org/licenses/BSD-2-Clause")
 
     fun fromString(url: String) = LicenseUrl(url)
   }
