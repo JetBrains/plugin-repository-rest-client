ea2d200b2ac84fa794955f78c4e1cff1950ed1ab
Prendota <Prendota@mail.ru>
2022-03-28 11:56:51 +0200

    chore(client): deprecated api
    GitOrigin-RevId: db8c109036a2aceb82983d694915ff7fdc09a05c

--- cli/src/main/kotlin/org/jetbrains/intellij/pluginRepository/client.kt
+++ cli/src/main/kotlin/org/jetbrains/intellij/pluginRepository/client.kt
@@ -7,154 +7,141 @@ import java.io.File
 import kotlin.system.exitProcess
 
 class Client {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            if (args.isEmpty()) {
-                System.err.println("Command is not specified: `upload`, `download`, `list` or `info` commands are supported.")
-                exitProcess(1)
-            }
-            val command = args[0]
-            val restParameters = args.copyOfRange(1, args.size)
-            when (command) {
-                "upload" -> upload(restParameters)
-                "download" -> exitProcess(if (download(restParameters) != null) 0 else 1)
-                "list" -> list(restParameters)
-                "info" -> info(restParameters)
-                else -> {
-                    System.err.println("Unknown command `$command`: `upload`, `download`, `list` or `info` commands are supported.")
-                    exitProcess(1)
-                }
-            }
+  companion object {
+    @JvmStatic
+    fun main(args: Array<String>) {
+      if (args.isEmpty()) {
+        System.err.println("Command is not specified: `upload`, `download`, `list` or `info` commands are supported.")
+        exitProcess(1)
+      }
+      val command = args[0]
+      val restParameters = args.copyOfRange(1, args.size)
+      when (command) {
+        "upload" -> upload(restParameters)
+        "download" -> exitProcess(if (download(restParameters) != null) 0 else 1)
+        "info" -> info(restParameters)
+        else -> {
+          System.err.println("Unknown command `$command`: `upload`, `download`, `list` or `info` commands are supported.")
+          exitProcess(1)
         }
+      }
+    }
 
-        private fun download(args: Array<String>): File? {
-            val options = DownloadOptions()
-            Args.parseOrExit(options, args)
+    private fun download(args: Array<String>): File? {
+      val options = DownloadOptions()
+      Args.parseOrExit(options, args)
 
-            if (options.version.isNullOrBlank() && options.ideBuild.isNullOrBlank()) {
-                System.err.print("`version` or `ide-build` must be specified")
-                exitProcess(1)
-            }
+      if (options.version.isNullOrBlank() && options.ideBuild.isNullOrBlank()) {
+        System.err.print("`version` or `ide-build` must be specified")
+        exitProcess(1)
+      }
 
-            val pluginRepository = PluginRepositoryFactory.create(
-              options.host).downloader
-            val channel = parseChannel(options.channel)
-            return if (!options.version.isNullOrBlank()) {
-              if(options.oldFile.isBlank()){
-                pluginRepository.download(options.pluginId!!, options.version!!, File(options.destination), channel)
-              }else{
-                pluginRepository.downloadViaBlockMap(options.pluginId!!, options.version!!, File(options.destination), File(options.oldFile), channel)
-              }
-            } else {
-              if(options.oldFile.isBlank()) {
-                pluginRepository.downloadLatestCompatiblePlugin(options.pluginId!!, options.ideBuild!!, File(options.destination),
-                  channel)
-              }else{
-                pluginRepository.downloadLatestCompatiblePluginViaBlockMap(options.pluginId!!, options.ideBuild!!, File(options.destination),
-                  File(options.oldFile), channel)
-              }
-            }
-
+      val pluginRepository = PluginRepositoryFactory.create(
+        options.host).downloader
+      val channel = parseChannel(options.channel)
+      return if (!options.version.isNullOrBlank()) {
+        if (options.oldFile.isBlank()) {
+          pluginRepository.download(options.pluginId!!, options.version!!, File(options.destination), channel)
+        } else {
+          pluginRepository.downloadViaBlockMap(options.pluginId!!, options.version!!, File(options.destination), File(options.oldFile), channel)
         }
-
-        private fun upload(args: Array<String>) {
-            val options = UploadOptions()
-            Args.parseOrExit(options, args)
-            val pluginRepository = PluginRepositoryFactory.create(
-              options.host, options.token).uploader
-            val pluginId = options.pluginId
-            when {
-                pluginId == null -> pluginRepository.uploadNewPlugin(File(options.pluginPath!!), 104,
-                                                                     "https://plugins.jetbrains.com/legal/terms-of-use", options.family!!)
-                pluginId.matches(Regex("\\d+")) -> pluginRepository.uploadPlugin(pluginId.toInt(), File(options.pluginPath!!), parseChannel(options.channel), options.notes)
-                else -> pluginRepository.uploadPlugin(pluginId, File(options.pluginPath!!), parseChannel(options.channel), options.notes)
-            }
+      } else {
+        if (options.oldFile.isBlank()) {
+          pluginRepository.downloadLatestCompatiblePlugin(options.pluginId!!, options.ideBuild!!, File(options.destination),
+            channel)
+        } else {
+          pluginRepository.downloadLatestCompatiblePluginViaBlockMap(options.pluginId!!, options.ideBuild!!, File(options.destination),
+            File(options.oldFile), channel)
         }
+      }
 
-        private fun list(args: Array<String>) {
-            val options = ListOptions()
-            Args.parseOrExit(options, args)
-            val pluginRepository = PluginRepositoryFactory.create(
-              options.host).pluginManager
-            val channel = parseChannel(options.channel)
-            val plugins = pluginRepository.listPlugins(options.ideBuild!!, channel, options.pluginId)
-            for (plugin in plugins) {
-                println("${plugin.name} (${plugin.id} version ${plugin.version}, IDE builds ${plugin.sinceBuild} to ${plugin.untilBuild}, depends ${plugin.depends}")
-            }
-        }
-
-        private fun info(args: Array<String>) {
-            val options = InfoOptions()
-            Args.parseOrExit(options, args)
-            val pluginRepository = PluginRepositoryFactory.create(
-              options.host).pluginManager
-            val plugin = pluginRepository.getPluginByXmlId(options.pluginId!!, options.family!!)
-            if (plugin != null) {
-                println("${plugin.name} ${plugin.id} made by ${plugin.vendor?.name}")
-            } else {
-                println("Plugin is not found!")
-            }
-        }
-
-        private fun parseChannel(channel: String?) = if (!channel.isNullOrEmpty() && channel != "_default_") channel else null
     }
 
-    class UploadOptions : BaseOptions() {
-        @set:Argument("plugin", required = false, description = "Plugin ID in the plugins repository or ID defined in plugin.xml")
-        var pluginId: String? = null
-
-        @set:Argument(required = true, description = "Hub permanent token")
-        var token: String? = null
-
-        @set:Argument("file", required = true, description = "Path to plugin zip/jar file")
-        var pluginPath: String? = null
-
-        @set:Argument("family", description = "Plugin's family")
-        var family: ProductFamily? = ProductFamily.INTELLIJ
-
-        @set:Argument(description = "Change notes (may include HTML tags). The argument is ignored when uploading updates for IntelliJ-based IDEs")
-        var notes: String? = null
+    private fun upload(args: Array<String>) {
+      val options = UploadOptions()
+      Args.parseOrExit(options, args)
+      val pluginRepository = PluginRepositoryFactory.create(
+        options.host, options.token).uploader
+      val pluginId = options.pluginId
+      when {
+        pluginId == null -> pluginRepository.uploadNewPlugin(File(options.pluginPath!!), 104,
+          "https://plugins.jetbrains.com/legal/terms-of-use", options.family!!)
+        pluginId.matches(Regex("\\d+")) -> pluginRepository.uploadPlugin(pluginId.toInt(), File(options.pluginPath!!), parseChannel(options.channel), options.notes)
+        else -> pluginRepository.uploadPlugin(pluginId, File(options.pluginPath!!), parseChannel(options.channel), options.notes)
+      }
     }
 
-    class DownloadOptions : BaseOptions() {
-        @set:Argument("plugin", required = true, description = "Plugin ID defined in plugin.xml")
-        var pluginId: String? = null
-
-        @set:Argument("version", description = "Plugin version to download")
-        var version: String? = null
-
-        @set:Argument("ide-build", description = "IDE build number with product code to download plugin compatible with (e.g. IC-145.184)")
-        var ideBuild: String? = null
-
-        @set:Argument("prev", description = "Previous plugin's version archive file path")
-        var oldFile: String = ""
-
-        @set:Argument("to", description = "Target filepath")
-        var destination: String = "."
+    private fun info(args: Array<String>) {
+      val options = InfoOptions()
+      Args.parseOrExit(options, args)
+      val pluginRepository = PluginRepositoryFactory.create(
+        options.host).pluginManager
+      val plugin = pluginRepository.getPluginByXmlId(options.pluginId!!, options.family!!)
+      if (plugin != null) {
+        println("${plugin.name} ${plugin.id} made by ${plugin.vendor?.name}")
+      } else {
+        println("Plugin is not found!")
+      }
     }
 
-    class ListOptions : BaseOptions() {
-        @set:Argument("ide-build", required = true, description = "IDE build number with product code to list plugins compatible with (e.g. IC-145.184)")
-        var ideBuild: String? = null
+    private fun parseChannel(channel: String?) = if (!channel.isNullOrEmpty() && channel != "_default_") channel else null
+  }
 
-        @set:Argument("plugin", description = "Plugin ID defined in plugin.xml")
-        var pluginId: String? = null
-    }
+  class UploadOptions : BaseOptions() {
+    @set:Argument("plugin", required = false, description = "Plugin ID in the plugins repository or ID defined in plugin.xml")
+    var pluginId: String? = null
 
-    class InfoOptions : BaseOptions() {
-        @set:Argument("plugin", required = true, description = "Plugin ID defined in plugin.xml")
-        var pluginId: String? = null
+    @set:Argument(required = true, description = "Hub permanent token")
+    var token: String? = null
 
-        @set:Argument("family", description = "Plugin's family")
-        var family: ProductFamily? = ProductFamily.INTELLIJ
-    }
+    @set:Argument("file", required = true, description = "Path to plugin zip/jar file")
+    var pluginPath: String? = null
 
-    open class BaseOptions {
-        @set:Argument(description = "Plugins repository host")
-        var host = "https://plugins.jetbrains.com"
+    @set:Argument("family", description = "Plugin's family")
+    var family: ProductFamily? = ProductFamily.INTELLIJ
 
-        @set:Argument(description = "Plugin channel")
-        var channel: String? = null
-    }
+    @set:Argument(description = "Change notes (may include HTML tags). The argument is ignored when uploading updates for IntelliJ-based IDEs")
+    var notes: String? = null
+  }
+
+  class DownloadOptions : BaseOptions() {
+    @set:Argument("plugin", required = true, description = "Plugin ID defined in plugin.xml")
+    var pluginId: String? = null
+
+    @set:Argument("version", description = "Plugin version to download")
+    var version: String? = null
+
+    @set:Argument("ide-build", description = "IDE build number with product code to download plugin compatible with (e.g. IC-145.184)")
+    var ideBuild: String? = null
+
+    @set:Argument("prev", description = "Previous plugin's version archive file path")
+    var oldFile: String = ""
+
+    @set:Argument("to", description = "Target filepath")
+    var destination: String = "."
+  }
+
+  class ListOptions : BaseOptions() {
+    @set:Argument("ide-build", required = true, description = "IDE build number with product code to list plugins compatible with (e.g. IC-145.184)")
+    var ideBuild: String? = null
+
+    @set:Argument("plugin", description = "Plugin ID defined in plugin.xml")
+    var pluginId: String? = null
+  }
+
+  class InfoOptions : BaseOptions() {
+    @set:Argument("plugin", required = true, description = "Plugin ID defined in plugin.xml")
+    var pluginId: String? = null
+
+    @set:Argument("family", description = "Plugin's family")
+    var family: ProductFamily? = ProductFamily.INTELLIJ
+  }
+
+  open class BaseOptions {
+    @set:Argument(description = "Plugins repository host")
+    var host = "https://plugins.jetbrains.com"
+
+    @set:Argument(description = "Plugin channel")
+    var channel: String? = null
+  }
 }
